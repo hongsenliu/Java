2675859551fc955666b5384d6da35c159a3dd317
hongsenliu <hongsenliu@gmail.com>
2013-10-12 02:54:32 -0400

    shove

--- src/shove/Shove.java
+++ src/shove/Shove.java
@@ -1,1 +1,22 @@
+package shove;
+public class Shove {
+       
+       /** Move A[A.length-1] so that it is just after the nearest 
+         *  previous item that is <= A[A.length-1], or to A[0] if 
+         *  there isn't such an item. Move all succeeding items 
+         *  to the right (i.e., up one index) . */
+
+	 static void moveOver(int[] A) {
+	     int current = A.length - 1;
+	     while (A[current] > A[current-1] && current > 0){
+		 int tmp = A[current-1];
+		 A[current-1] = A[current];
+		 A[current-1] = tmp;
+		 current--;
+	     }
+	 }
+    /** The final position of ELT in B. */
+    static int position(int[] B, int elt) {
+	return 0;
+    }
 }
