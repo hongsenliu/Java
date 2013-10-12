930bc639dcec2695a03d46a2cdbd5e8e6088b7b0
hongsenliu <hongsenliu@gmail.com>
2013-10-12 03:16:08 -0400

    fix loop

--- src/shove/Shove.java
+++ src/shove/Shove.java
@@ -1,22 +1,30 @@
 package shove;
 public class Shove {
-       
-       /** Move A[A.length-1] so that it is just after the nearest 
-         *  previous item that is <= A[A.length-1], or to A[0] if 
-         *  there isn't such an item. Move all succeeding items 
-         *  to the right (i.e., up one index) . */
+    
+	public static void main(String[] args) {
+		int[] A = new int[args.length];
+		for (int i = 0; i < args.length; i++) {
+			A[i] = Integer.parseInt(args[i]);
+		}
+		moveOver(A);
+		for (int s : A) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+	}
+	/** Move A[A.length-1] so that it is just after the nearest 
+     *  previous item that is <= A[A.length-1], or to A[0] if 
+     *  there isn't such an item. Move all succeeding items 
+     *  to the right (i.e., up one index) . */
 
 	 static void moveOver(int[] A) {
 	     int current = A.length - 1;
-	     while (A[current] > A[current-1] && current > 0){
+	     while (A[current] < A[current-1] && current > 0){
 		 int tmp = A[current-1];
 		 A[current-1] = A[current];
-		 A[current-1] = tmp;
+		 A[current] = tmp;
 		 current--;
 	     }
 	 }
-    /** The final position of ELT in B. */
-    static int position(int[] B, int elt) {
-	return 0;
-    }
+    
 }
